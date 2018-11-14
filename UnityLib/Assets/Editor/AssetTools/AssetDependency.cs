f78cff14a0ef1abf3260534516e179b7b98e62b3
kyle <zh880517@163.com>
2018-11-14 14:41:49 +0800

    添加Json库和资源依赖信息

---
+++ UnityLib/Assets/Editor/AssetTools/AssetDependency.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetDependency
+{
+    public string Path { get; private set; }
+    public string[] Dependencies { get; private set; }
+    public string DependenciesHash { get; private set; }
+
+    public AssetDependency(string path)
+    {
+        Path = path;
+        Update();
+    }
+
+    public AssetDependency()
+    {
+         
+    }
+
+    public bool Update()
+    {
+        if (!File.Exists(Path))
+            return false;
+        var hash = AssetDatabase.GetAssetDependencyHash(Path).ToString();
+        if (hash != DependenciesHash)
+        {
+            DependenciesHash = hash;
+            Dependencies = AssetDatabase.GetDependencies(Path);
+        }
+        return true;
+    }
+}
---
+++ UnityLib/Assets/Editor/EditorTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class EditorTest
+{
+    [MenuItem("Tools/工具/测试")]
+    public static void Test()
+    {
+        
+    }
+}
