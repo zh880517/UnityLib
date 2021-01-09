679f26a36899a65afa11ff555d311dd81a645f72
张丹 <zhangdan@lvbinggame.com>
2021-01-09 17:27:17 +0800

    添加资源获取辅助工具

---
+++ UnityLib/Assets/StateGraph/Editor/Common/EditorResources.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class EditorResources
+{
+    private static readonly Dictionary<string, Object> resources = new Dictionary<string, Object>();
+
+    public static T Get<T>(string path) where T : Object
+    {
+        if (!resources.ContainsKey(path))
+        {
+            var resource = Resources.Load<T>(path);
+            resources.Add(path, resource);
+        }
+
+        return resources[path] as T;
+    }
+}
